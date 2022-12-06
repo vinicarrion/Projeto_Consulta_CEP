e2f62e2eddbe702e382fcd43691852891ab094b9
Vinicius Mitsuo Carvalho Carrion Moises <37308081+vinicarrion@users.noreply.github.com>
2022-12-06 13:26:22 -0300

    Update App.js

--- App_Buscar_CEP/App.js
+++ App_Buscar_CEP/App.js
@@ -1,16 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View, Alert} from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, Alert, Animation} from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default function App() {
   const [cep, setCep] = useState("")
-  const[endereco, setEndereco] = useState(null)
+  const [endereco, setEndereco] = useState(null)
+  const [carregando, setCarregando] = useState(false)
 
 const buscarCep = () => {
+
+  //Validacao de CEP
+
+  if(cep.replace("-", "").length != 8){
+    alert('CEP invalido')
+    return 
+  }
+  
+  setCarregando(true)
   fetch(`https://viacep.com.br/ws/${cep}/json/`)
   .then(resposta => resposta.json())
   .then(obj => setEndereco(obj))
-  .catch(erro => alert(erro))
+  .catch(() => alert(`Nao encontrado o CEP ${cep}`))
+  .finally(() => setCarregando(false))
 }
 
   return (
@@ -18,10 +30,16 @@ const buscarCep = () => {
       <Text style={styles.Titulo}>Consultar Endereço</Text>
       <Text style={styles.subTitulo}>Digite o CEP</Text>
       <TextInput style={styles.input} value={cep} onChangeText={input => setCep(input)}/>
-      <Button title='Buscar' onPress={buscarCep}/>
+      <Button style={styles.button} title='Buscar' onPress={buscarCep}/>
+      {carregando && <LottieView source={require('./animations/loading.json')} style={{width: 40, height: 200}} loop={false} ref={Animation}/>}
       {endereco != null && (
-        <View>
-          <Text>CEP{endereco.cep}</Text>
+        <View>      
+          <Text>CEP: {endereco.cep}</Text>
+          <Text>Logradouro: {endereco.logradouro}</Text>
+          <Text>Complemento: {endereco.complemento}</Text>
+          <Text>Bairro: {endereco.bairro}</Text>
+          <Text>Cidade: {endereco.bairro}</Text>
+          <Text>UF: {endereco.uf}</Text>
           </View>
       )}
       <StatusBar style="auto" />
